c066c7e9cfd79ece202abf9047c2a9b21a2c27ee
wudih146 <wudih146126@163.com>
2020-01-09 21:09:57 +0800

    test

--- app/src/main/java/com/example/gongkongjie/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/example/gongkongjie/ui/dashboard/DashboardViewModel.java
@@ -6,14 +6,14 @@ import androidx.lifecycle.ViewModel;
 
 public class DashboardViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+private MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+public DashboardViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is dashboard fragment");
-    }
+        }
 
-    public LiveData<String> getText() {
+public LiveData<String> getText() {
         return mText;
-    }
-}
+        }
+        }
